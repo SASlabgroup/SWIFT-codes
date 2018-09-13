f30af58286f969ad1b44c03463d5a804a3795c67
Jim Thomson <41773462+jthomson-apluw@users.noreply.github.com>
2018-09-12 22:05:54 -0800

    simple mapping function (#8)
    new function to map variables (determined by input field name)

---
+++ GeneralTools/mapSWIFT.m
@@ -0,0 +1,57 @@
+function h = mapSWIFT(colorfield);
+% make a map of SWIFT drift vectors
+%   for a given set of SWIFT files in a directory
+%   (i.e., for a given mission/project/day)
+% color the drift vectors by the fieldcolor input
+%   such as temperature, salinity, or even just time
+%
+%  h = mapSWIFT(colorfield);
+%
+% where colorfield is string with the field name to use for the colors
+% and returns h as the handle to the figure
+%
+% J. Thomson, 9/2018
+
+h = figure(1); clf
+
+flist = dir('*SWIFT*.mat');
+
+wd = pwd;
+lastslash = find(wd=='/',1,'last') + 1;
+wd = wd( lastslash : end );
+
+for fi = 1:length(flist),
+    
+    load(flist(fi).name)
+    clear color
+    
+    if isfield(SWIFT,colorfield),
+        
+        for si = 1:length(SWIFT)
+            color(si) = max( getfield(SWIFT(si),colorfield) ); % use max incase of multiple values (i.e., 3 CTs)
+        end
+            
+            h = scatter([SWIFT.lon],[SWIFT.lat],60,color,'filled'); 
+            
+            hold on
+            
+            u = [SWIFT.driftspd] .* sind([SWIFT.driftdirT]);
+            v = [SWIFT.driftspd] .* cosd([SWIFT.driftdirT]);
+            
+            quiver([SWIFT.lon],[SWIFT.lat],u,v,'k-');
+            
+        else,
+            
+            disp([colorfield ' not found in SWIFT data structure'])
+            
+        end
+        
+end
+   
+set(gca,'color',[.8 .8 .8])
+title([wd ' ' colorfield ],'interp','none')
+colorbar
+xlabel('lon')
+ylabel('lat')
+grid
+print('-dpng',[wd colorfield '_map.png']) 
