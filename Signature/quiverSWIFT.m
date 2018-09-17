f1cdb9c896086dca988ab6e2b5a8d56546a548bc
Jim Thomson <41773462+jthomson-apluw@users.noreply.github.com>
2018-09-17 14:39:17 -0800

    update quiverSWIFT
    add input for quiver scale, plus curtain plots

--- Signature/quiverSWIFT.m
+++ Signature/quiverSWIFT.m
@@ -1,17 +1,20 @@
-function h = quiverSWIFT(colorfield);
+function h = quiverSWIFT(colorfield, quiverscale);
 % plot SWIFT current profiles with adjustments for drift
 % ... and maybe for Stokes (to be implemented later)
 %   for a given set of SWIFT files in a directory
 %   (i.e., for a given mission/project/day)
 
-%  h = quiverSWIFT(colorfield);
+%  h = quiverSWIFT(colorfield, quiverscale);
 %
 % where colorfield is string with the field name to use for the colors
-% and returns h as the handle to the figure
+% and quiverscale adjusts the arrow size (0.01 is recommended)
+% and returns h as the handle to the figures
 %
 % J. Thomson, 9/2018
 
-h = figure; clf
+h(1) = figure(1); clf
+h(2) = figure(2); clf
+ax(1) = subplot(2,1,1); ax(2) = subplot(2,1,2);
 
 flist = dir('*SWIFT*.mat');
 
@@ -20,7 +23,7 @@ lastslash = find(wd=='/',1,'last') + 1;
 wd = wd( lastslash : end );
 
 for fi=1:length(flist),
-    
+    disp(flist(fi).name)
     load(flist(fi).name)
     clear u v w x y z color
     
@@ -42,8 +45,17 @@ for fi=1:length(flist),
             
         end
         
-        quiver3(x,y,z,u,v,w,.5,'k'), hold on
-        scatter([SWIFT.lon],[SWIFT.lat],50,color), colorbar
+        figure(1)
+        quiver3(x,y,z,u,v,w,quiverscale,'k'), hold on
+        scatter([SWIFT.lon],[SWIFT.lat],50,color,'filled'), colorbar
+        
+        figure(2)
+        axes(ax(1))
+        scatter3(x(:),y(:),z(:),10,u(:),'filled'), hold on
+        title('east velocity [m/s]'), caxis([-.5 .5]), colorbar
+        axes(ax(2))
+        scatter3(x(:),y(:),z(:),10,v(:),'filled'), hold on
+        title('north velocity [m/s]'), caxis([-.5 .5]), colorbar
         
     else
         disp('No signature data.  Someone should expand this to work on AQD profiles too.')
@@ -54,16 +66,36 @@ end
 
 %% 
 
+figure(1)
 xlabel('lon')
 ylabel('lat')
 zlabel('z [m]')
 title(['SWIFT ' wd ' ' colorfield])
 grid
-ratio = [1./abs(cosd(nanmean([SWIFT.lat]))),1,100];  % ratio of lat to lon distances at a given latitude
-daspect(ratio)
+%ratio = [1./abs(cosd(nanmean([SWIFT.lat]))),1,100];  % ratio of lat to lon distances at a given latitude
+%daspect(ratio)
 set(gca,'ZDir','reverse')
 grid
-
 print('-dpng',[wd '_quiverSWIFT_' colorfield '.png'])
+
+figure(2)
+%ratio = [1./abs(cosd(nanmean([SWIFT.lat]))),1,100];  % ratio of lat to lon distances at a given latitude
+axes(ax(1))
+%daspect(ratio)
+xlabel('lon')
+ylabel('lat')
+zlabel('z [m]')
+set(gca,'ZDir','reverse')
+grid
+axes(ax(2))
+%daspect(ratio)
+xlabel('lon')
+ylabel('lat')
+zlabel('z [m]')
+set(gca,'ZDir','reverse')
+grid
+hlink = linkprop(ax,{'CameraPosition','CameraUpVector'})
+rotate3d on
+print('-dpng',[wd '_curtainSWIFT.png'])
 
 end
