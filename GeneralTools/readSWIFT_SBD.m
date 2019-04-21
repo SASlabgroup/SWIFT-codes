d40b44486f708d451fb2d7b1604fb463f117104f
Jim Thomson <41773462+jthomson-apluw@users.noreply.github.com>
2019-04-21 16:23:24 -0400

    Update SBD tools
    More display messages to understand when burst data is empty or bursts are being screened (removed).

--- GeneralTools/readSWIFT_SBD.m
+++ GeneralTools/readSWIFT_SBD.m
@@ -36,17 +36,18 @@ function [SWIFT BatteryVoltage ] = readSWIFT_SBD( fname , plotflag );
 %             9/2018    switched to only filling fields where/when data is
 %                       present ** which will require updating plotSWIFT
 %                       using isfield(SWIFT,'xxxxx') to make sure there is
-%                       something to plot ** 
+%                       something to plot **
 %                       The only required fields will be time, lat, and lon
 %
 %             9/2018    accomodating a bug in onboard signature processing,
-%                       which reads HR velocities as cm/s instead of m/s 
+%                       which reads HR velocities as cm/s instead of m/s
 %                        and thereby scales dissipation rates by 10^(8/3)
 %
-% 
-%   M. Smith  9/2018    added CTdepth and metheight fields
-%                       added SWIFT.id field using sbd filename 
 %
+%   M. Smith  9/2018    added CTdepth and metheight fields
+%                       added SWIFT.id field using sbd filename
+%   J. Thomson 4/2019   correct Sig xcdr depth (0.2 m) and fill empty Signature results with NaNs.
+%                       move Airmar PB200 read to after SBG, so can fill missing positions
 
 recip = true; % binary flag to change wave direction to FROM
 
@@ -56,6 +57,7 @@ SWIFT.lon = [];
 
 fid = fopen(fname); % open file for reading
 BatteryVoltage = NaN; % placeholder
+SWIFTversion = []; % placeholder
 
 %% SWIFT id flag from file name
 SWIFT.ID = fname(6:13);
@@ -99,57 +101,7 @@ while 1
         cells = length(SWIFT.downlooking.velocityprofile);
         z = blanking + res./2 + [0:(cells-1)]*res;
         SWIFT.downlooking.z = z; %fliplr(z) - blanking;
-        
-    elseif type == 2 & size > 0, % PB200 weather station (Airmar) and backup GPS
-        disp('reading Airmar results')
-        SWIFT.winddirT = fread(fid,1,'float'); % mean wind direction (deg T)
-        SWIFT.winddirTstddev =  fread(fid,1,'float'); % std dev of wind direction (deg)
-        SWIFT.windspd = fread(fid,1,'float'); % mean wind speed (m/s)
-        SWIFT.windspdstddev = fread(fid,1,'float');  % std dev of wind spd (m/s)
-        PBlat = num2str(fread(fid,1,'float')); % latitude
-        PBlon = num2str(fread(fid,1,'float'));  % longitude
-        if ~isfield(SWIFT,'lat'), % if no IMU, use this one
-            SWIFT.lat = PBlat;
-            SWIFT.lon = PBlon;
-        end
-        if isfield(SWIFT,'lat')& SWIFT.lat == 0, % if IMU did not give position, use this one
-            SWIFT.lat = PBlat;
-            SWIFT.lon = PBlon;
-        else
-        end
-        PByear = num2str(fread(fid,1,'uint32')); % year
-        PBMonth = num2str(fread(fid,1,'uint32')); % month
-        PBDay = num2str(fread(fid,1,'uint32'));  % day
-        PBhhmmss = num2str(fread(fid,1,'uint32')); % time of day (UTC)
-        dec = find( PBhhmmss == '.');
-        if dec == 7, hour = str2num(PBhhmmss(1:2));,
-        elseif dec == 6, hour = str2num(PBhhmmss(1));, mm = str2num(PBhhmmss(2:3));,
-        elseif dec <=5, hour = 0;
-        elseif isempty(dec) & length(PBhhmmss)==6, hour = str2num(PBhhmmss(1:2));, mm = str2num(PBhhmmss(3:4));,
-        elseif isempty(dec) & length(PBhhmmss)==5, hour = str2num(PBhhmmss(1));, mm = str2num(PBhhmmss(2:3));,
-        elseif isempty(dec) & length(PBhhmmss)==4, hour = 0;, mm = str2num(PBhhmmss(1:2));,
-        elseif isempty(dec) & length(PBhhmmss)==3, hour = 0;, mm = str2num(PBhhmmss(1));,
-        else hour = 0; mm = 0;
-        end
-        SWIFT.time = datenum( str2num(PByear), str2num(PBMonth), str2num(PBDay), hour, mm, 0);
-        SWIFT.date = [PBDay PBMonth PByear];
-        SWIFT.airtemp = fread(fid,1,'float');
-        SWIFT.airtempstddev = fread(fid,1,'float');
-        SWIFT.airpres = fread(fid,1,'float');
-        SWIFT.airpresstddev = fread(fid,1,'float');
-        SWIFT.driftdirT = fread(fid,1,'float');
-        SWIFT.driftdirTstddev = fread(fid,1,'float');
-        SWIFT.driftspd = fread(fid,1,'float');
-        SWIFT.driftspdstddev = fread(fid,1,'float');
-        if PBlat == 0, % if no GPS for AirMar PB200, winddir is relative, not true
-            SWIFT.winddirR = SWIFT.winddirT;
-            SWIFT.winddirT = NaN;
-        else
-            SWIFT.winddirR = NaN;
-            SWIFT.winddirRstddev = NaN;
-        end
-        SWIFT.metheight = 0.84; % height of measurement, meters
-        
+          
     elseif type == 3 & size > 0, % IMU
         disp('reading Microstrain IMU results')
         SWIFT.sigwaveheight = fread(fid,1,'float'); % sig wave height
@@ -231,7 +183,7 @@ while 1
         SWIFT.rainaccum = fread(fid,1,'float'); % millimeters
         SWIFT.rainint = fread(fid,1,'float'); % millimeters_per_hour
         SWIFT.metheight = 0.84; % height of measurement, meters
-
+        
     elseif type == 9 & size > 0, % Nortek Signature
         disp('reading Nortek Signature results, applying cm/s correction')
         cmcorrection = 10^(8/3);
@@ -240,7 +192,7 @@ while 1
         SWIFT.signature.HRprofile.tkedissipationrate = fread(fid,ncells,'float') * cmcorrection; % turbulent dissipation rate in m^2/s^3
         res = 0.04; % cell size (m) from config file
         blanking = 0.10; % blanking distance (m) from config file
-        depth = 0.15; % depth of transducer
+        depth = 0.2; % depth of transducer
         cells = length(SWIFT.signature.HRprofile.tkedissipationrate);
         SWIFT.signature.HRprofile.z = blanking + res./2 + [0:(cells-1)]*res;
         % broadband profile (slant beams)
@@ -249,15 +201,17 @@ while 1
         SWIFT.signature.profile.north = fread(fid,nAvgcells,'float'); % east velocity component (m/s)
         res = 0.5; % cell size (m) from config file
         blanking = 0.10; % blanking distance (m) from config file
-        depth = 0.15; % depth of transducer
+        depth = 0.2; % depth of transducer
         cells = length(SWIFT.signature.profile.east);
         SWIFT.signature.profile.z = blanking + res./2 + [0:(cells-1)]*res;
-        if isempty(SWIFT.signature.HRprofile.tkedissipationrate),
-            SWIFT = rmfield( SWIFT, 'signature' );
-            disp('Signature results are empty!'),
-        else 
-        end
-        
+    elseif type == 9 & size == 0, % Nortek Signature present, but empty results
+        disp('Signature results are empty!')
+        SWIFT.signature.HRprofile.tkedissipationrate = NaN(64,1);
+        SWIFT.signature.HRprofile.z = NaN(1,64); 
+        SWIFT.signature.profile.east = NaN(40,1);
+        SWIFT.signature.profile.north = NaN(40,1);
+        SWIFT.signature.profile.z = NaN(1,40);
+    
     elseif type == 10 & size > 0, % SBG Ellipse (same as IMU in v3, but with check factor and no histograms)
         disp('reading SBG IMU results'),
         SWIFT.sigwaveheight = fread(fid,1,'float'); % sig wave height
@@ -283,40 +237,101 @@ while 1
         end
         SWIFTversion = 4;
         
-    elseif type == 11 & size > 0, % RM Young 8100 Sonic Anemometer
-        disp('reading Sonic Anemometer results')
-        SWIFT.windustar = fread(fid,1,'float'); % wind friction velocity
-        SWIFT.windepsilon = fread(fid,1,'float'); % air-side tke dissipation rate
-        SWIFT.windmeanu = fread(fid,1,'float'); % component mean
-        SWIFT.windmeanv = fread(fid,1,'float'); % component mean
-        SWIFT.windspd = sqrt( SWIFT.windmeanu.^2 + SWIFT.windmeanv.^2 ); % relative wind speed
-        SWIFT.windmeanw = fread(fid,1,'float'); % component mean
-        SWIFT.airtemp = fread(fid,1,'float'); % airtemp
-        SWIFT.windanisotropy = fread(fid,1,'float'); % inertial sub-range ratio
-        SWIFT.windustarquality = fread(fid,1,'float'); % quality of inertial spectral fit
-        SWIFT.windspectra.freq = fread(fid,116,'float'); % frequency
-        SWIFT.windspectra.energy = fread(fid,116,'float'); % spectral energy density of surface winds
-        SWIFT.winddirR = rad2deg(atan2(SWIFT.windmeanv, SWIFT.windmeanu) ); % mean wind direction (deg relative)        
-        SWIFT.metheight = 0.71; % height of measurement, meters
         
-    elseif type == 12 & size > 0, % Oxygen optode
-        disp('reading Oxygen optode results')
-        SWIFT.O2conc = fread(fid,1,'float');
+        elseif type == 2 & size > 0, % PB200 weather station (Airmar) and backup GPS
+        disp('reading Airmar results')
+        SWIFT.winddirT = fread(fid,1,'float'); % mean wind direction (deg T)
+        SWIFT.winddirTstddev =  fread(fid,1,'float'); % std dev of wind direction (deg)
+        SWIFT.windspd = fread(fid,1,'float'); % mean wind speed (m/s)
+        SWIFT.windspdstddev = fread(fid,1,'float');  % std dev of wind spd (m/s)
+        PBlat = num2str(fread(fid,1,'float')); % latitude *** STILL NEEDS TO BE PARSED (like time str)
+        PBlon = num2str(fread(fid,1,'float'));  % longitude *** STILL NEEDS TO BE PARSED (like time str)
+        if ~isfield(SWIFT,'lat'), % if no IMU, use this one
+            disp('Using Airmar positions')
+            SWIFT.lat = PBlat;
+            SWIFT.lon = PBlon;
+        end
+        if isfield(SWIFT,'lat') & ( SWIFT.lat == 0 | isempty(SWIFT.lat) ), % if IMU did not give position, use this one
+            disp('Using Airmar positions')
+            SWIFT.lat = PBlat;
+            SWIFT.lon = PBlon;
+        else
+        end
+        PByear = num2str(fread(fid,1,'uint32')); % year
+        PBMonth = num2str(fread(fid,1,'uint32')); % month
+        PBDay = num2str(fread(fid,1,'uint32'));  % day
+        PBhhmmss = num2str(fread(fid,1,'uint32')); % time of day (UTC)
+        dec = find( PBhhmmss == '.');
+        if dec == 7, hour = str2num(PBhhmmss(1:2));,
+        elseif dec == 6, hour = str2num(PBhhmmss(1));, mm = str2num(PBhhmmss(2:3));,
+        elseif dec <=5, hour = 0;
+        elseif isempty(dec) & length(PBhhmmss)==6, hour = str2num(PBhhmmss(1:2));, mm = str2num(PBhhmmss(3:4));,
+        elseif isempty(dec) & length(PBhhmmss)==5, hour = str2num(PBhhmmss(1));, mm = str2num(PBhhmmss(2:3));,
+        elseif isempty(dec) & length(PBhhmmss)==4, hour = 0;, mm = str2num(PBhhmmss(1:2));,
+        elseif isempty(dec) & length(PBhhmmss)==3, hour = 0;, mm = str2num(PBhhmmss(1));,
+        else hour = 0; mm = 0;
+        end
+        SWIFT.time = datenum( str2num(PByear), str2num(PBMonth), str2num(PBDay), hour, mm, 0);
+        SWIFT.date = [PBDay PBMonth PByear];
+        SWIFT.airtemp = fread(fid,1,'float');
+        SWIFT.airtempstddev = fread(fid,1,'float');
+        SWIFT.airpres = fread(fid,1,'float');
+        SWIFT.airpresstddev = fread(fid,1,'float');
+        SWIFT.driftdirT = fread(fid,1,'float');
+        SWIFT.driftdirTstddev = fread(fid,1,'float');
+        SWIFT.driftspd = fread(fid,1,'float');
+        SWIFT.driftspdstddev = fread(fid,1,'float');
+        if PBlat == 0, % if no GPS for AirMar PB200, winddir is relative, not true
+            SWIFT.winddirR = SWIFT.winddirT;
+            SWIFT.winddirT = NaN;
+        else
+            SWIFT.winddirR = NaN;
+            SWIFT.winddirRstddev = NaN;
+        end
+        if ~isempty(SWIFTversion) && SWIFTversion==3,
+            SWIFT.metheight = 0.84; % height of measurement, meters
+        elseif ~isempty(SWIFTversion) && SWIFTversion==4,
+            SWIFT.metheight = 0.4; % height of measurement, meters
+        else
+            SWIFT.metheight = 0.4; % height of measurement, meters
+        end
+
+      
         
-    elseif type == 13 & size > 0, % SeaOwl
-        disp('reading SeaOWl results')
-        SWIFT.FDOM = fread(fid,1,'float');
-              
-    else
-        
-    end  
-    
-    if isempty(type),% & size == 0,
-        %disp('----------')
-        disp('all done (nothing else in this file)')
-        break,
-    end
-    
+        elseif type == 11 & size > 0, % RM Young 8100 Sonic Anemometer
+            disp('reading Sonic Anemometer results')
+            SWIFT.windustar = fread(fid,1,'float'); % wind friction velocity
+            SWIFT.windepsilon = fread(fid,1,'float'); % air-side tke dissipation rate
+            SWIFT.windmeanu = fread(fid,1,'float'); % component mean
+            SWIFT.windmeanv = fread(fid,1,'float'); % component mean
+            SWIFT.windspd = sqrt( SWIFT.windmeanu.^2 + SWIFT.windmeanv.^2 ); % relative wind speed
+            SWIFT.windmeanw = fread(fid,1,'float'); % component mean
+            SWIFT.airtemp = fread(fid,1,'float'); % airtemp
+            SWIFT.windanisotropy = fread(fid,1,'float'); % inertial sub-range ratio
+            SWIFT.windustarquality = fread(fid,1,'float'); % quality of inertial spectral fit
+            SWIFT.windspectra.freq = fread(fid,116,'float'); % frequency
+            SWIFT.windspectra.energy = fread(fid,116,'float'); % spectral energy density of surface winds
+            SWIFT.winddirR = rad2deg(atan2(SWIFT.windmeanv, SWIFT.windmeanu) ); % mean wind direction (deg relative)
+            SWIFT.metheight = 0.71; % height of measurement, meters
+            
+            elseif type == 12 & size > 0, % Oxygen optode
+                disp('reading Oxygen optode results')
+                SWIFT.O2conc = fread(fid,1,'float');
+                
+                elseif type == 13 & size > 0, % SeaOwl
+                    disp('reading SeaOWl results')
+                    SWIFT.FDOM = fread(fid,1,'float');
+                    
+                    else
+                        
+end
+
+if isempty(type),% & size == 0,
+    %disp('----------')
+    disp('all done (nothing else in this file)')
+    break,
+end
+
 end
 fclose(fid);
 
@@ -339,11 +354,11 @@ if isfield(SWIFT,'windspectra'),
     fmax = 3; % Hz
     K = 0.55 ; % Kolmogorov const, factor by 4/3 for vertical or cross-flow component
     kv = 0.4 ; % von Karman const
-    z = 0.95; % meters above sea level, 0.77 for Gill on Waveglider, 0.95 for RM Young on SWIFT 
+    z = 0.95; % meters above sea level, 0.77 for Gill on Waveglider, 0.95 for RM Young on SWIFT
     inertialrange = find( SWIFT.windspectra.freq > fmin & SWIFT.windspectra.freq < fmax);
     inertiallevel = nanmean( SWIFT.windspectra.energy(inertialrange) .* (SWIFT.windspectra.freq(inertialrange)).^(5/3) ) ;
     epsilon =  ( inertiallevel ./ ( ( SWIFT.windspd ./ (2*pi) ).^(2/3)  .* K ) ).^(3/2);
-    SWIFT.windustar = (kv * epsilon * z ).^(1/3) ./ 2;  % assumes neutral 
+    SWIFT.windustar = (kv * epsilon * z ).^(1/3) ./ 2;  % assumes neutral
 else
 end
 
@@ -354,10 +369,10 @@ if isfield(SWIFT,'CTdepth')
             SWIFT.CTdepth = .66;
         elseif SWIFTversion == 4 %v4, uses SBG waves
             SWIFT.CTdepth = .18;
-        else 
+        else
             SWIFT.CTdepth = nan;
         end
-    elseif length(SWIFT.CTdepth) == 2 
+    elseif length(SWIFT.CTdepth) == 2
         SWIFT.CTdepth = nan(2,1);
     end
 end
@@ -365,7 +380,7 @@ end
 %% quality control onboard processing of SBG IMU wave directional momements
 % which are indicated by momemnts larger than +/- 1 (improper normalization)
 % this has been fixed in Oct 2017 version of onboard processing, but it's
-% harmless to keep this keep screening in place 
+% harmless to keep this keep screening in place
 % (in case of rereading pre-2017 sbd files)
 if isfield(SWIFT,'wavespectra'),
     if any(abs(SWIFT.wavespectra.b1)>1) | any(abs(SWIFT.wavespectra.a1)>1),
