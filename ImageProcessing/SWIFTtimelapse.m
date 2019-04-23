e306e97ff5b29401afda6259a045b5de95779068
Jim Thomson <41773462+jthomson-apluw@users.noreply.github.com>
2019-04-22 21:48:15 -0400

    new script to make time-lapse movies (with timestamps)

---
+++ ImageProcessing/SWIFTtimelapse.m
@@ -0,0 +1,71 @@
+% script to pull all images from a SWIFT deployment
+% and make a movie, incuding timestamps
+% works on both mast and hull cameras (different coms)
+% ** must be run from the mission directory (card offload) **
+%
+% J. Thomson, 4/2019
+
+clear all, close all
+tic
+
+com = 'COM-7'; % 7 is mast and 9 is hull (on v4 SWIFTs)
+missiondir = pwd
+wd = pwd;
+lastslash = find(wd=='/',1,'last') + 1;
+wd = wd( lastslash : end );
+
+% initialize movie
+fast = false;  % fast means no printed timestamps
+vidObj = VideoWriter([wd '_video' com '.avi']);
+open(vidObj);
+
+cd([ com '/Raw/' ]);
+
+daydirs = dir('*');
+
+for di = 1:length(daydirs)
+    
+    if ~strcmp( daydirs(di).name(1), '.' )
+        
+        cd([ daydirs(di).name  ] ), pwd
+        
+        hrdirs = dir('*');
+        
+        for hi = 1:length(hrdirs)
+            
+            if ~strcmp( hrdirs(hi).name(1), '.' )
+                
+                cd([ hrdirs(hi).name  ]), pwd
+                
+                % list and load images
+                filelist = dir('*.jpg');
+                
+                for fi = 1:length(filelist),
+                    pic = imread(filelist(fi).name,'jpg');
+                    if fast % fast version, no timestamps
+                        currFrame = im2frame(pic);
+                    else % slow version, with timestamps
+                        image(pic),
+                        text(10,10,[filelist(fi).name],'interpreter','none')
+                        currFrame = getframe(gcf);
+                    end
+                    writeVideo(vidObj,currFrame);
+                    
+                end
+                
+                cd('../')
+                
+            else end
+            
+        end
+        
+        cd('../')
+        
+    else end
+end
+
+cd([ missiondir ])
+
+close(vidObj);
+
+toc
