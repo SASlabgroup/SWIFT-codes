debceb0a6a10158f5e4dcba0afdc4aab34265f17
Jim Thomson <41773462+jthomson-apluw@users.noreply.github.com>
2019-04-23 22:12:48 -0400

    Signature altimeter and backscatter
    Use altimeter to trim velocity profiles, and make backscatter plots

--- Signature/reprocess_SIG.m
+++ Signature/reprocess_SIG.m
@@ -5,12 +5,18 @@
 %
 %
 % J. Thomson, Sept 2017 (modified from AQH reprocessing)
-%       Jul 2018, fix bug in the burst time stamp applied 
-
+%       7/2018, fix bug in the burst time stamp applied 
+%       4/2019, apply altimeter results to trim profiles
+%               and plot echograms
 
 clear all; close all
+
+tic
+
 parentdir = pwd;
-readraw = false;
+readraw = false; % reading the binaries doubles the run time
+altimetertrim = true;
+xcdrdepth = 0.2; % depth of transducer [m]
 
 mincor = 50; % correlation cutoff, 50 recommended (max value recorded in air), 30 if single beam acq
 
@@ -39,7 +45,7 @@ for di = 1:length(dirlist),
         
        disp(['file ' num2str(fi) ' of ' num2str(length(filelist)) ])
         
-        if filelist(fi).bytes > 2e6,
+        if filelist(fi).bytes > 1e6%2e6,
             
             % read or load raw data
             if isempty(dir([filelist(fi).name(1:end-4) '.mat'])) | readraw,
@@ -53,7 +59,7 @@ for di = 1:length(dirlist),
             burst.VelocityData(exclude)  = NaN;
             
             % recalc dissipation
-            z = 0.2 + burst.Blanking + burst.CellSize * [1:size(burst.VelocityData,2)];
+            z = xcdrdepth + burst.Blanking + burst.CellSize * [1:size(burst.VelocityData,2)];
             deltar = zeros(size(z));
             [tke , epsilon , residual, A, Aerror, N, Nerror] = dissipation(burst.VelocityData', z, size(burst.VelocityData,1), 0, deltar);
             %epsilon = epsilon./1024;
@@ -64,15 +70,30 @@ for di = 1:length(dirlist),
             avgwbar = mean(avg.VelocityData(:,:,3));
             avgwvar = var(avg.VelocityData(:,:,3));
             
+            % use altimeter dist, if present, to trim profiles
+            profilez = xcdrdepth + avg.Blanking + avg.CellSize./2 + ( avg.CellSize * [1:size(avg.VelocityData,2)] );
+            if isfield(avg,'AltimeterDistance') && altimetertrim, 
+                maxz = median(avg.AltimeterDistance);
+                trimbin = find( profilez > maxz, 1) - 1;
+                if trimbin < 1, trimbin = 1; end
+                avgwbar(trimbin:end) = NaN;
+                avgwvar(trimbin:end) = NaN;
+            else
+                maxz = inf;
+            end
+            
             % match time to SWIFT structure and replace values
             time=datenum(filelist(fi).name(13:21))+datenum(0,0,0,str2num(filelist(fi).name(23:24)),(str2num(filelist(fi).name(26:27))-1)*12,0);            
             [tdiff tindex] = min(abs([SWIFT.time]-time));
             SWIFT(tindex).signature.HRprofile.wbar = HRwbar;
             SWIFT(tindex).signature.HRprofile.wvar = HRwvar;
+            SWIFT(tindex).signature.HRprofile.tkedissipationrate_pp = epsilon;
             SWIFT(tindex).signature.profile.wbar = avgwbar;
             SWIFT(tindex).signature.profile.wvar = avgwvar;
-            SWIFT(tindex).signature.HRprofile.tkedissipationrate_pp = epsilon;
-            
+            SWIFT(tindex).signature.profile.east(trimbin:end) = NaN;
+            SWIFT(tindex).signature.profile.north(trimbin:end) = NaN;
+            SWIFT(tindex).signature.profile.altimeter = maxz;
+   
             figure(1), clf 
             plot(HRwbar,SWIFT(tindex).signature.HRprofile.z,'b-'), hold on
             plot(HRwbar+sqrt(HRwvar),SWIFT(tindex).signature.HRprofile.z,'b:'),
@@ -88,9 +109,6 @@ for di = 1:length(dirlist),
             
             figure(2), clf 
             semilogx(SWIFT(tindex).signature.HRprofile.tkedissipationrate,SWIFT(tindex).signature.HRprofile.z,'k-'), hold on
-            %semilogx(SWIFT(tindex).signature.HRprofile.tkedissipationrate*10^(8/3),SWIFT(tindex).signature.HRprofile.z,'b-'), hold on
-            %semilogx(SWIFT(tindex).signature.HRprofile.tkedissipationrate*10^(4),SWIFT(tindex).signature.HRprofile.z,'g-'), hold on
-            %legend('onboard','onboard, mm/s correction','onboard, cm/s correction','post-processed','Location','NorthEastOutside')
             semilogx(epsilon,z,'rx'), hold on
             legend('onboard','post-processed','Location','NorthEastOutside')
             xlabel('\epsilon [W/Kg]'),ylabel('z [m]')
@@ -98,6 +116,26 @@ for di = 1:length(dirlist),
             drawnow, 
             print('-dpng',[filelist(fi).name(1:end-4) '_disspation.png'])
             
+            figure(3), clf
+            subplot(2,1,1)
+            pcolor(burst.time,z,burst.AmplitudeData'),shading flat
+            set(gca,'Ydir','reverse')
+            colorbar
+            drawnow, 
+            datetick
+            ylabel('z [m]')
+            title([filelist(fi).name(1:end-4) '   backscatter'],'interpreter','none'),
+            ax = axis;
+            subplot(2,1,2)
+            pcolor(avg.time,profilez,mean(avg.AmplitudeData,3)'), shading flat, hold on
+            plot(avg.time,avg.AltimeterDistance,'k.'), 
+            set(gca,'Ydir','reverse')
+            colorbar
+            drawnow, 
+            datetick
+            axis([ax(1) ax(2) min(profilez) max(profilez)])
+            ylabel('z [m]')
+            print('-dpng',[filelist(fi).name(1:end-4) '_backscatter.png'])
         else
         end
         
@@ -111,3 +149,4 @@ cd(parentdir)
 
 save([ wd '_reprocessedSIG.mat'],'SWIFT')
 
+toc
