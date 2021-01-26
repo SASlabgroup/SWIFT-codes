035e16245b6948b895e569ac101f5c834d7e1dfc
Jim Thomson <41773462+jthomson-apluw@users.noreply.github.com>
2021-01-26 08:26:00 -0800

    complementary filter for IMU angles

--- microSWIFT/microSWIFT_motion.m
+++ microSWIFT/microSWIFT_motion.m
@@ -1,23 +1,32 @@
 function ENU = microSWIFT_motion(IMU);
-% Matlab for for microSWIFT motion processing
-% transform onboard IMU to motion in earth reference frame
-% which has local coordinates and associated Euler angles: roll, pitch, yaw
-% x: along bottle (towards cap)
-% y: accross bottle (right hand sys)
-% z: up (skyward, same as GPS)
+% Matlab for for microSWIFT motion processing,
+% transforming onboard IMU to motion in earth reference frame
+%
+% IMU local coordinates and associated Euler angles are:
+%   x: along bottle (towards cap), roll around this axis
+%   y: accross bottle (right hand sys), pitch around this axis
+%   z: up (skyward, same as GPS), yaw around this axis
 %
 %   ENU = microSWIFT_motion(IMU);
 %
 % using data already in IMU structure from function "readmicroSWIFT_IMU.m"
 %
-% ** should compare this with "ahrsfilter" from Matlab sensor fusion toolbox 
+% Key step is usinh a complimentary filter to
+% combines static estimates of orientation from accelerometer data
+% with dynamic estimates of orientation from gyro data
+% then rotates everything into the horizontal earth plane
+% then uses horizontal magnememeter components to determine heading 
+% and rotate again to geographic yaw, 
+% then integrates the ENU accelerations to get XYZ wave displacements
+%
+% all makes heavy use of a high-pass RC filter (embedded function) 
+% which reduces drift in the time integration of gryos and accelerations
+%
 %
 % J. Thomson, 10/2020
 %              1/2021 add heading from magnetometers, fix roll-pitch confusions
 %
-% TO DO: get sampling freq from IMU timestamps directly
-%        confirm gyro axes
-%        check horizontal x,y motion 
+%
 %
 
 %% constants
@@ -28,8 +37,12 @@ M = [-0.076;  -0.013;  0; ]; % position vector of IMU relative to buoy center [m
 
 MagOffsets = [60; 60; 120;]; % magnetometer offsets
 
+Wd = 0; % weighting (0 to 1) of dynamic angles in complimentary filter
+Ws = 1-Wd; % weighting (0 to 1) for static angles in complimentary filter
+
 fs =  length(IMU.acc)./512; % sampling freq (Hz)
 dt = 1./fs;
+
 
 %% despike (might be uneccesary)
 
@@ -37,73 +50,90 @@ IMU.acc = filloutliers(IMU.acc,'linear');
 IMU.angles = filloutliers(IMU.angles,'linear');
 IMU.gyro = filloutliers(IMU.gyro,'linear');
 
+%% data from structure
 
-%% loop thru timesteps, going from body frame to earth frame
+ax = IMU.acc(:,1); % acc in x [m/s]
+ay = IMU.acc(:,2); % acc in y [m/s]
+az = IMU.acc(:,3); % acc in z [m/s]
+gx = IMU.gyro(:,1); % gryo in x [deg/s]
+gy = IMU.gyro(:,2); % gryo in y [deg/s]
+gz = IMU.gyro(:,3); % gryo in z [deg/s]
+mx = IMU.mag(:,1); % magnetometer in x [uT]
+my = IMU.mag(:,2); % magnetometer in y [uT]
+mz = IMU.mag(:,3); % magnetometer in z [uT]
 
-for ii=1:length(IMU.clock) 
-    
+
+%% estimate pitch and roll with complementary filter
+
+staticroll = mean( atan2d(ay, az) ); % roll around x axis [deg] in absence of linear acceleration
+staticpitch = mean( atan2d(-ax, sqrt(ay.^2 + az.^2 ) ) ); % pitch around y axis [deg] in absence of linear acceleration 
+staticyaw = 0; % yaw around z axis [deg], zero for now... it's relative until applying the magnetometer 
+
+dynamicroll = cumtrapz(gx)*dt; % time integrate to get dynamic roll
+dynamicroll = RCfilter(dynamicroll, RC, fs); % high-pass filter to reduce drift
+dynamicpitch = cumtrapz(gy)*dt; % time integrate to get dynamic roll
+dynamicpitch = RCfilter(dynamicpitch, RC, fs); % high-pass filter to reduce drift
+dynamicyaw = cumtrapz(gz)*dt; % time integrate to get dynamic yaw
+dynamicyaw = RCfilter(dynamicyaw, RC, fs); % high-pass filter to reduce drift
+
+% combine orientation estimates (using weighted complementary filter)
+roll = Wd*dynamicroll + Ws*staticroll;
+pitch = Wd*dynamicpitch + Ws*staticpitch;
+yaw = Wd*dynamicyaw + Ws*staticyaw;
+
+ENU.angles(:,1) = roll;
+ENU.angles(:,2) = pitch;
+ENU.angles(:,3) = yaw;
+
 %% make rotation matrix
 % careful here, order of Euler angles matters, see Zippel 2018 (JPO) and Edson 1998 for clues
-% biggest issue is assumption of weak linear accelerations, such that
-% gravity is the reference in getting roll and pitch
-%
-% note that the onboard python code had these Euler angles as follows: 
-%   roll = 180 * math.atan(accel_x/math.sqrt(accel_y*accel_y + accel_z*accel_z))/math.pi
-%   pitch = 180 * math.atan(accel_y/math.sqrt(accel_x*accel_x + accel_z*accel_z))/math.pi
-%   yaw = 180 * math.atan(accel_z/math.sqrt(accel_x*accel_x + accel_y*accel_y))/math.pi
-% which swapped roll and pitch... and made a big damn headache
 
-%r = atan2d(IMU.acc(ii,2), IMU.acc(ii,3)); % rotation around x axis
-r = atan2d(IMU.acc(ii,2), sqrt(IMU.acc(ii,1)^2 + IMU.acc(ii,3).^2 ) ); % rotation around x axis
-p = atan2d(IMU.acc(ii,1), sqrt(IMU.acc(ii,2)^2 + IMU.acc(ii,3).^2 ) ); % rotaion around y axis
-y = 0;
-% p = IMU.angles(ii,1);
-% r = IMU.angles(ii,2);
-% y = IMU.angles(ii,3);
-
-% yaw matrix
-Y = [cosd(y) -sind(y) 0;...
-     sind(y) cosd(y) 0;...
-        0       0    1;];
+for ii=1:length(IMU.clock)
     
-% pitch matrix
-P = [cosd(p) 0  -sind(p);...
-        0    1    0    ;...
-     sind(p) 0  cosd(p);];
-       
-% roll matrix
-R = [   1     0         0   ;...    
-        0   cosd(r) -sind(r);...
-        0   sind(r)  cosd(r);];
-        
-
-% transformation matrix from buoy to earth reference frame 
-T = Y * (P * R);
-
-
-%% rotate linear accelerations and magnetomer readings to earth frame (but not heading, that comes later)
-ENU.acc(ii,:) = T * IMU.acc(ii,:)'; 
-ENU.mag(ii,:) = T * IMU.mag(ii,:)'; 
-
-%% create the angular rate matrix in earth frame
-% (rotate the "strapped-down" gyro measurements from body to earth frame)
-% lingering issue is units... gyro supposed to be rad/s, 
-% but suspect is might be deg/s based on spec sheet with range -/+ 250 
-% (which is min/max on x channel)
-
-rrate = IMU.gyro(ii,1);
-prate = IMU.gyro(ii,2);
-yrate = IMU.gyro(ii,3);
-
-Omega = [0; 0; yrate;]  +    Y*[0; prate; 0;]    +  Y*P*[rrate; 0; 0;];
-
-Omega = deg2rad(Omega);
-
-
-%% calc velocities from rotation (to remove before wave processing)
-
-ROT.vel(ii,:) = cross(Omega, T*M);
-
+    r = roll(ii); 
+    p = pitch(ii);
+    y = yaw(ii);
+    
+    % yaw matrix
+    Y = [cosd(y) -sind(y) 0;...
+         sind(y) cosd(y)  0;...
+         0       0        1;];
+    
+    % pitch matrix
+    P = [cosd(p) 0  sind(p);...
+        0        1  0      ;...
+        -sind(p) 0  cosd(p);];
+    
+    % roll matrix
+    R = [1   0        0       ;...
+         0   cosd(r)  -sind(r);...
+         0   sind(r)  cosd(r);];
+    
+    
+    % transformation matrix from buoy to earth reference frame
+    T = Y * (P * R);
+    %T = R * P * Y;
+    %T = inv(T);
+    
+    
+    %% rotate linear accelerations and magnetomer readings to horizontal (earth frame)
+    ENU.acc(ii,:) = T * IMU.acc(ii,:)';
+    ENU.mag(ii,:) = T * IMU.mag(ii,:)';
+    
+    %% create the angular rate matrix in earth frame
+    % (rotate the "strapped-down" gyro measurements from body to earth frame)
+    
+    Omega = [0; 0; gz(ii);]  +    Y*[0; gy(ii); 0;]    +  Y*P*[gx(ii); 0; 0;];
+    
+    Omega = deg2rad(Omega);
+    
+    
+    %% calc velocities from rotation (to remove before wave processing)
+    % this has negligible effect, because M distances are so small
+    % probably should exclude in onboard processing (for computational speed)
+    
+    ROT.vel(ii,:) = cross(Omega, T*M);
+    
 end % close loop thru time steps
 
 disp('mean acceleration values in Earth reference frame')
@@ -120,7 +150,7 @@ ENU.vel = cumtrapz(ENU.acc)*dt; % m/s
 
 %% remove rotation-induced velocities from total velocity
 
-ENU.vel = ENU.vel - ROT.vel;
+ENU.vel = ENU.vel - ROT.vel; % doesn't seem to matter much, because M distance are small
 
 %% determine geographic heading and correct horizontal velocities to East, North
 
@@ -129,9 +159,9 @@ ENU.heading(ENU.heading<0) = 360+ENU.heading(ENU.heading<0);
 theta = -(ENU.heading - 90); % cartesian CCW heading from geographic CW heading
 
 u = ENU.vel(:,1); % x dir (horizontal in earth frame, but relative in azimuth)
-v = ENU.vel(:,1); % y dir (horizontal in earth frame, but relative in azimuth)
-ENU.vel(:,1) = u.*cosd(theta) + v.*cosd(theta); % east component
-ENU.vel(:,2) = -u.*cosd(theta) + y.*cosd(theta); % north compoent
+v = ENU.vel(:,2); % y dir (horizontal in earth frame, but relative in azimuth)
+ENU.vel(:,1) = u.*cosd(theta) - v.*sind(theta); % east component
+ENU.vel(:,2) = u.*sind(theta) + v.*cosd(theta); % north compoent
 
 %% filter and integrate velocity for displacements, and filter again
 
@@ -153,15 +183,15 @@ ENU.pos(1:round(RC./dt*10),:) = NaN;
 
 %% EMBEDDED RC FILTER function (high pass filter) %%
 
-function a = RCfilter(b, RC, fs); 
-
-alpha = RC / (RC + 1./fs); 
-a(1,:) = b(1,:);
-
-for ui = 2:length(b)
-    a(ui,:) = alpha * a(ui-1,:) + alpha * ( b(ui,:) - b(ui-1,:) );
-end
-
-end
+    function a = RCfilter(b, RC, fs);
+        
+        alpha = RC / (RC + 1./fs);
+        a(1,:) = b(1,:);
+        
+        for ui = 2:length(b)
+            a(ui,:) = alpha * a(ui-1,:) + alpha * ( b(ui,:) - b(ui-1,:) );
+        end
+        
+    end
 
 end
