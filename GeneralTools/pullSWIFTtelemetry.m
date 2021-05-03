aaeeac7dc7506caaf30c6551b58688496203e7f6
Jim Thomson <41773462+jthomson-apluw@users.noreply.github.com>
2021-05-03 16:32:54 -0700

    microSWIFT compatibility in pullSWIFTtelemetry.m

--- GeneralTools/pullSWIFTtelemetry.m
+++ GeneralTools/pullSWIFTtelemetry.m
@@ -13,86 +13,108 @@ function [ allbatterylevels lasttime lastlat lastlon] = pullSWIFTtelemetry( IDs,
 % note that this puts everything in the current working directory
 %
 %   J. Thomson, 9/2018
+%           5/2021 make compatible with microSWIFTs (three digit IDs)
 
 global battery
 
-options = weboptions('Timeout', 20);
+options = weboptions('Timeout', 60);
 
 
 if  size(IDs,2) == 2 && ischar(IDs), % enforce two digit strings for SWIFT IDs
     
+    SWIFTtype = 'v3v4s'
     allbatterylevels = NaN(1,length(IDs)); % initialize battery array
     lasttime =  NaN(1,length(IDs)); % initialize time array
-    lastlat = NaN(1,length(IDs)); % initialize 
-    lastlon = NaN(1,length(IDs)); % initialize 
+    lastlat = NaN(1,length(IDs)); % initialize
+    lastlon = NaN(1,length(IDs)); % initialize
     
+elseif  size(IDs,2) == 3 && ischar(IDs), % enforce two digit strings for SWIFT IDs
     
-    %% loop thru pulling SBDs for each SWIFT ID
-    
-    for si=1:length(IDs),
-        
-        baseurl = 'http://swiftserver.apl.washington.edu/services/buoy?action=get_data&buoy_name=';
-        buoy = ['SWIFT%20' IDs(si,:) ];
-        out = websave(['SWIFT' IDs(si,:) '.zip'],[baseurl buoy  '&start=' starttime '&end=' endtime '&format=zip'],options)
-        
-        eval(['!unzip SWIFT' IDs(si,:) '.zip']);
-        expanded = dir(['*SWIFT ' IDs(si,:) '*']);
-        if length(expanded)==1 && expanded(1).isdir == true,
-            cd(expanded(1).name)
-            
-            % run compile SBD, which calls readSWIFT_SBD for each file
-            % use a temp file work around the clear all in compile
-            save temp si IDs starttime endtime options allbatterylevels lasttime lastlon lastlat
-            compileSWIFT_SBDservertelemetry
-            load temp
-            wd = pwd;
-            wdi = find(wd == '/',1,'last');
-            wd = wd((wdi+1):end);
-            if ~isempty(SWIFT),
-                allbatterylevels(si) = battery(end);
-                lasttime(si) = max([SWIFT.time]);
-                lastlat(si) = SWIFT(end).lat;
-                lastlon(si) = SWIFT(end).lon;
-            else
-                eval(['!rm '  wd '.mat']), % remove file if no results
-                allbatterylevels(si) = NaN;
-                lasttime(si) = 0;
-                lastlat(si) = NaN;
-                lastlon(si) = NaN;
-            end
-            
-            % keep going to next SWIFT
-            cd('../')
-            
-        else
-            disp(['multiple expanded directories for SWIFT ' IDs(si,:)])
-            allbatterylevels(si) = NaN;
-            lasttime(si) = 0;
-            lastlat(si) = NaN;
-            lastlon(si) = NaN; 
-        end
-        
-    end
-    
-    %% combine the resulting mat files in the top level directory and make map plots
-    eval(['!cp *SWIFT*/*SWIFT*start*.mat ./'])
-    tempfig = mapSWIFT('watertemp');
-    salinityfig = mapSWIFT('salinity');
-    wavefig = mapSWIFT('sigwaveheight');
-    
-    clc
-    spaces(1:length(IDs)) = ' ';
-    commas(1:length(IDs)) = ',';
-    Vs(1:length(IDs)) = 'V';
-    [IDs commas' spaces' datestr(lasttime) commas' spaces' num2str(lastlat',6) commas' spaces' ...
-        num2str(lastlon',6) commas' spaces' num2str(allbatterylevels',3) Vs']
-    
-    
+    SWIFTtype = 'micro'
+    allbatterylevels = NaN(1,length(IDs)); % initialize battery array
+    lasttime =  NaN(1,length(IDs)); % initialize time array
+    lastlat = NaN(1,length(IDs)); % initialize
+    lastlon = NaN(1,length(IDs)); % initialize
     
 else
     
-    disp('input IDs must be two digit strings')
+    allbatterylevels = NaN(1,length(IDs)); % initialize battery array
+    lasttime =  NaN(1,length(IDs)); % initialize time array
+    lastlat = NaN(1,length(IDs)); % initialize
+    lastlon = NaN(1,length(IDs)); % initialize
+    
+    disp('invalid IDs')
     
     return
     
 end
+
+%% loop thru pulling SBDs for each SWIFT ID
+
+for si=1:length(IDs),
+    
+    baseurl = 'http://swiftserver.apl.washington.edu/services/buoy?action=get_data&buoy_name=';
+    
+    if SWIFTtype =='v3v4s'
+        buoy = ['SWIFT%20' IDs(si,:) ];
+    elseif SWIFTtype =='micro'
+        buoy = ['microSWIFT%20' IDs(si,:) ];
+    else
+    end
+    
+    out = websave(['SWIFT' IDs(si,:) '.zip'],[baseurl buoy  '&start=' starttime '&end=' endtime '&format=zip'],options)
+    
+    eval(['!unzip SWIFT' IDs(si,:) '.zip']);
+    expanded = dir(['*SWIFT ' IDs(si,:) '*']);
+    if length(expanded)==1 && expanded(1).isdir == true,
+        cd(expanded(1).name)
+        
+        % run compile SBD, which calls readSWIFT_SBD for each file
+        % use a temp file work around the clear all in compile
+        save temp si IDs starttime endtime options allbatterylevels lasttime lastlon lastlat SWIFTtype
+        compileSWIFT_SBDservertelemetry
+        load temp
+        wd = pwd;
+        wdi = find(wd == '/',1,'last');
+        wd = wd((wdi+1):end);
+        if ~isempty(SWIFT),
+            allbatterylevels(si) = battery(end);
+            lasttime(si) = max([SWIFT.time]);
+            lastlat(si) = SWIFT(end).lat;
+            lastlon(si) = SWIFT(end).lon;
+        else
+            eval(['!rm '  wd '.mat']), % remove file if no results
+            allbatterylevels(si) = NaN;
+            lasttime(si) = 0;
+            lastlat(si) = NaN;
+            lastlon(si) = NaN;
+        end
+        
+        % keep going to next SWIFT
+        cd('../')
+        
+    else
+        disp(['multiple expanded directories for SWIFT ' IDs(si,:)])
+        allbatterylevels(si) = NaN;
+        lasttime(si) = 0;
+        lastlat(si) = NaN;
+        lastlon(si) = NaN;
+    end
+    
+end
+
+%% combine the resulting mat files in the top level directory and make map plots
+eval(['!cp *SWIFT*/*SWIFT*start*.mat ./'])
+tempfig = mapSWIFT('watertemp');
+salinityfig = mapSWIFT('salinity');
+wavefig = mapSWIFT('sigwaveheight');
+
+clc
+spaces(1:length(IDs)) = ' ';
+commas(1:length(IDs)) = ',';
+Vs(1:length(IDs)) = 'V';
+[IDs commas' spaces' datestr(lasttime) commas' spaces' num2str(lastlat',6) commas' spaces' ...
+    num2str(lastlon',6) commas' spaces' num2str(allbatterylevels',3) Vs']
+
+
+
