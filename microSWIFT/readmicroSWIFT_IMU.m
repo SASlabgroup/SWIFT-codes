5a24a3d230e20e6063938d8bd6d466e2e5bbda4d
Jacob Davis <63017854+jacobrdavis@users.noreply.github.com>
2021-12-14 09:20:00 -0800

    modified datenum creation in readmicroSWIFT_IMU (#16)

--- microSWIFT/readmicroSWIFT_IMU.m
+++ microSWIFT/readmicroSWIFT_IMU.m
@@ -20,9 +20,28 @@ IMU.gyro = data.data(:,7:9);
 %IMU.angles = data.data(:,10:12);  % removed in fall 2020
 
 %% timestamps
-for i=1:length(IMU.clock), 
-    IMU.time(i) = datenum(IMU.clock(i));
-end
+IMU.time= datenum(IMU.clock); % modified 12/2021
+
+%%% J. Davis 12/2021:
+%%% Leaving this chunk of code here temporarily if you're interested in 
+%%% doing the time test yourself. Added the new statement to line 23 above.
+%%% also: please check if a transposition is needed; the original statement
+%%% outputs a row vector, but the new statement outputs a column vector
+%%% (which matches the remaining IMU fields anyway).
+
+% tic
+% for i=1:length(IMU.clock)
+%     IMU.time(i) = datenum(IMU.clock(i));
+% end
+% toc
+% % takes ~32s on my machine
+% tic
+% IMU.time2= datenum(IMU.clock);
+% toc
+% % takes ~2s on my machine
+% sum(IMU.time.'-IMU.time2) % will be zero if equivalent
+
+%%% (end of test)
 
 %% plots
 
