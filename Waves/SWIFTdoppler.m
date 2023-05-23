4ae2a836bcd6496d0ddb73786d0bdb442d20bbd4
Jim Thomson <jthomson@apl.washington.edu>
2023-05-22 22:01:00 -0700

    update SWIFTdoppler
    bug fix

--- Waves/SWIFTdoppler.m
+++ Waves/SWIFTdoppler.m
@@ -8,17 +8,17 @@ function SWIFT = SWIFTdoppler(SWIFT);
 % J. Thomson Feb 2017
 %
 
-for si=1:length(SWIFT); 
-    
+for si=1:length(SWIFT)
     
     % determine compoents of drift
     ueast = SWIFT(si).driftspd .* cosd(SWIFT(si).driftdirT);
     unorth = SWIFT(si).driftspd .* sind(SWIFT(si).driftdirT);
     
     % determine components of vector k
-    k = (6.28 .* SWIFT(si).wavespectra.freq).^2 ./ 9.8; % deep water dispersion
+    f = SWIFT(si).wavespectra.freq;
+    k = (6.28 .* f).^2 ./ 9.8; % deep water dispersion
     
-    [Etheta theta f dir spread spread2 spread2alt ] = SWIFTdirectionalspectra(SWIFT(si), 0);
+    [Etheta theta f dir spread spread2 spread2alt ] = SWIFTdirectionalspectra(SWIFT(si), 0, 1);
 
     keast  = k .* cosd(dir);
     knorth = k .* cosd(dir);
@@ -26,7 +26,7 @@ for si=1:length(SWIFT);
     % Doppler shift
     shift = ueast.*keast + unorth.*knorth;
     
-    SWIFT(si).wavespectra.omega = 6.28*SWIFT(si).wavespectra.freq + shift;
+    SWIFT(si).wavespectra.omega = SWIFT(si).wavespectra.freq + shift;
     
 end
 
