b7893937f0ae930f7c593fe960266c12060dda1a
kzeiden <github2917@shroud.cc>
2024-01-01 13:41:27 -0800

    Update to hannwinPSD2
    Save phase information

--- Signature/hannwinPSD2.m
+++ Signature/hannwinPSD2.m
@@ -1,4 +1,4 @@
-function [PS,F,err] = hannwinPSD2(data,M,fs,norm)
+function [PS,F,PH,err] = hannwinPSD2(data,M,fs,norm)
 % hannwinPSD2 Computes spectra via windowing
 % PS is either unitless (if normalized to integrate to 1) or units of
 % variance of data (e.g. m^2/s^2 for velocity (m/s)).
@@ -10,23 +10,28 @@ nwin = 2*(floor(N/M))-1; %Number of windows (%50 overlap)
 h = sqrt(8/3)*hann(M);% Hanning window variance preservation
 
 PS = NaN(nwin,floor(M/2));
+PH = NaN(nwin,floor(M/2));
 for i=1:nwin 
     k1 = (i-1)*M/2 + 1;
     k2 = (i+1)*M/2;
     datai = data(k1:k2);
-    St = datai;
-    St(isnan(St)) = nanmean(St); % Replace NaN w/mean
-    St = St - nanmean(St); % Remove mean
-    St = detrend(St); % Remove trend
+    ts = datai;
+    ts(isnan(ts)) = nanmean(ts); % Replace NaN w/mean
+    ts = ts - nanmean(ts); % Remove mean
+    ts = detrend(ts); % Remove trend
     %(IMPORTANT TO REMOVE MEAN BEFORE WINDOW!)
-    St = h.*St'; % Window using normalized hanning window
-    Sp = (abs(fft(St)).^2);
-    Sp = Sp(1:floor(end/2));
-    Sp(2:end) = 2*Sp(2:end);
-    PS(i,:) = Sp;
+    ts = h.*ts'; % Window using normalized hanning window
+    ph = angle(fft(ts));
+    ps = (abs(fft(ts)).^2);
+    ph = ph(1:floor(end/2));
+    ps = ps(1:floor(end/2));
+    ps(2:end) = 2*ps(2:end);
+    PS(i,:) = ps;
+    PH(i,:) = ph;
 end
 %Average
 PS = nanmean(PS,1);
+PH = nanmean(PH,1);
 
 %Frequencies Computed
 T = M/fs; 
