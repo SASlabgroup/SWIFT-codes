7c2b75814f01edab81ecc5941f421939dceca2b8
kzeiden <github2917@shroud.cc>
2024-01-03 10:54:10 -0800

    updates

--- Signature/plotSIGburst.m
+++ Signature/plotSIGburst.m
@@ -17,24 +17,18 @@ MP = get(0,'monitorposition');
 set(gcf,'outerposition',MP(1,:).*[1 1 1 1]);
 
 subplot(5,1,1)
-plot(1:nping,burst.pitch,'k','LineWidth',2)
-hold on
-plot(1:nping,burst.roll,'color',rgb('grey'),'LineWidth',2)
 plot(1:nping,tilt,'r','LineWidth',2)
-plot(1:nping,zeros(1,nping),'--k')
-plot(1:nping,-20*ones(1,nping),':k')
+hold on
 plot(1:nping,20*ones(1,nping),':k')
-title('Orientation')
+title('Tilt')
 c = colorbar;c.Visible = 'off';
 ylabel('\Theta [deg]')
-l = legend('Pitch','Roll','Tilt');
 l.Position(1:2) = c.Position(1:2);
 
 subplot(5,1,2)
 imagesc(1:nping,1:nbin,burst.AmplitudeData')
 title('Amplitude')
 cmocean('haline');caxis([75 175])
-set(gca,'YDir','normal')
 c = colorbar;c.Label.String = 'A [counts]';
 ylabel('Bin #')
 
@@ -42,7 +36,6 @@ subplot(5,1,3)
 imagesc(1:nping,1:nbin,burst.CorrelationData')
 title('Correlation')
 cmocean('thermal');caxis([50 100])
-set(gca,'YDir','normal')
 c = colorbar;c.Label.String = 'C [%]';
 ylabel('Bin #')
 
@@ -50,7 +43,6 @@ subplot(5,1,4)
 imagesc(1:nping,1:nbin,burst.VelocityData')
 title('Velocity')
 cmocean('balance');caxis([-0.5 0.5])
-set(gca,'YDir','normal')
 c = colorbar;c.Label.String = 'W [ms^{-1}]';
 ylabel('Bin #')
     
