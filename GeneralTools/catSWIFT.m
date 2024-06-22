4244260fa08813d25a33d41850f64df8989b9e36
kzeiden <97851010+kzeiden@users.noreply.github.com>
2024-06-22 15:16:45 -0800

    Update catSWIFT.m
    modification to handle funky sizes and empty bursts in SWIFT

--- GeneralTools/catSWIFT.m
+++ GeneralTools/catSWIFT.m
@@ -1,6 +1,13 @@
 
-function [swift,fh] = catSWIFT(SWIFT)
+function swift = catSWIFT(SWIFT)
 % Returns concatenated swift data in structure format
+
+% ID
+swift.ID = SWIFT(1).ID;
+
+% Met Height + CT depth
+swift.metheight = SWIFT(1).metheight;
+swift.ctdepth = SWIFT(1).CTdepth;
 
 %Time, lat, lon, battery
 swift.time = [SWIFT.time];
@@ -13,13 +20,12 @@ swift.battery = [SWIFT.battery];
 end
 
 % Air & sea temperature & pressure & salinity
-if isfield(SWIFT,'airtemp')
-swift.tair = [SWIFT.airtemp];
+if isfield(SWIFT,'watertemp')
+    for it = 1:nt
+        swift.tsea(it) = max(SWIFT(it).watertemp);
+    end
 else
-    swift.tair = NaN(1,nt);
-end
-for it = 1:nt
-swift.tsea(it) = max(SWIFT(it).watertemp);
+    swift.tsea = NaN(1,nt);
 end
 if isfield(SWIFT,'airtemp')
 swift.tair = [SWIFT.airtemp];
@@ -33,6 +39,31 @@ else
 end
 if isfield(SWIFT,'airpres')
     swift.press = [SWIFT.airpres];
+else
+    swift.press = NaN(1,nt);
+end
+
+% Rain
+if isfield(SWIFT,'rainaccum')
+    swift.rain = [SWIFT.rainaccum];
+else
+    swift.rain = NaN(1,nt);
+end
+
+% Humidity
+if isfield(SWIFT,'relhumidity')
+    swift.humid = [SWIFT.relhumidity];
+else
+    swift.humid = NaN(1,nt);
+end
+
+% Radiometer
+if isfield(SWIFT,'radiometertemp1mean')
+    nrad = length(SWIFT(1).radiometertemp1mean);
+    swift.radtemp1 = reshape([SWIFT.radiometertemp1mean],nrad,length(SWIFT))';
+    swift.radtemp2 = reshape([SWIFT.radiometertemp2mean],nrad,length(SWIFT))';
+    swift.rad1 = reshape([SWIFT.radiometerrad1],nrad,length(SWIFT))';
+    swift.rad2 = reshape([SWIFT.radiometerrad2],nrad,length(SWIFT))';
 end
 
 % Drift velocity
@@ -116,7 +147,6 @@ swift.subu = swift.relu + swift.driftu;
 swift.subv = swift.relv + swift.driftv;
 
 %Waves
-% SWIFT = SWIFT_Stokes(SWIFT);
 for it = 1:nt
     wavepower = SWIFT(it).wavespectra.energy;
     wavefreq = SWIFT(it).wavespectra.freq;
@@ -173,7 +203,9 @@ swift.wavepeakT = 1./(waveweight./wavevar);
     nf = length(swift.windfreq);
     swift.windpower = NaN(nf,nt);
     for it = 1:nt
+        if ~isempty(SWIFT(it).windspectra.energy)
      swift.windpower(:,it) = SWIFT(it).windspectra.energy;
+        end
     end
  else
     swift.windfreq = NaN(116,1);
@@ -207,150 +239,4 @@ elseif isfield(SWIFT,'uplooking')
     swift.surftke(1:4,:) = NaN;% Deepest three bins are bad 
 end
 
-%% Plot 
-
-fh = figure('color','w','Name',['SWIFT' SWIFT(1).ID]);
-lw = 2;
-fullscreen
-h = tight_subplot(8,3,[0.03 0.05],[0.125 0.1],0.075);
-
-% Anemometer
-axes(h(1))
-plot(swift.time,swift.windu,'-rx','LineWidth',lw)
-ylabel('U [ms^{-1}]')
-title('Wind Speed')
-axes(h(4))
-plot(swift.time,swift.tair,'-rx','LineWidth',lw)
-ylabel('T_{air} [^{\circ}C]')
-title('Air Temperature')
-
-%IMU/GPS
-axes(h(7))
-plot(swift.time,swift.wavesigH,'-bx','LineWidth',lw)
-ylabel('H_s [m]')
-title('Significant Wave Height')
-axes(h(10))
-plot(swift.time,swift.wavepeakT,'-bx','LineWidth',lw)
-ylabel('T_p [s]')
-title('Peak Wave Period')
-axes(h(13))
-plot(swift.time,swift.wavepeakdir,'-bx','LineWidth',lw)
-ylabel('\Theta [^{\circ}]')
-title('Peak Wave Direction')
-axes(h(16))
-plot(swift.time,swift.tsea,'-gx','LineWidth',lw)
-ylabel('T_{sea} [^{\circ}C]')
-title('Sea Temperature')
-axes(h(19))
-plot(swift.time,swift.sal,'-gx','LineWidth',lw)
-ylabel('S [psu]')
-title('Salinity')
-axes(h(22))
-plot([SWIFT.time],[SWIFT.battery],'-kx','linewidth',lw)
-ylabel('[V]')
-title('Battery Level')
-
-% ADCP
-axes(h(5))
-ax = gca;ax.Position = ax.Position.*[1 1 1 2.1]+[-0.01 0.025 0 0];
-imagesc(swift.time,swift.depth,swift.relu);
-c = slimcolorbar;
-c.Label.String = 'U_r [ms^{-1}]';
-cmocean('balance')
-clim([-0.25 0.25])
-ylabel('Z [m]')
-title('Zonal Velocity (Relative)')
-axes(h(11))
-ax = gca;ax.Position = ax.Position.*[1 1 1 2.1]+[-0.01 0.03 0 0];
-imagesc(swift.time,swift.depth,swift.relv);
-c = slimcolorbar;
-c.Label.String = 'V_r [ms^{-1}]';
-cmocean('balance')
-clim([-0.25 0.25])
-ylabel('Z [m]')
-title('Merid. Velocity (Relative)')
-
-if isfield(swift,'surftke')
-axes(h(23))
-ax = gca;ax.Position = ax.Position.*[1 1 1 4.2]+[-0.01 0 0 0];
-imagesc(swift.time,swift.surfz,log10(swift.surftke))
-c = slimcolorbar;
-c.Label.String = '\epsilon [m^2s^{-3}]';
-c.Location = 'NorthOutside';
-clim([-6 -3])
-ylabel('Z [m]')
-title('Dissipation Rate (0-5 m, Wave Biased)')
-end
-
-% Trajectory
-axes(h(12))
-ax = gca;ax.Position = ax.Position.*[1 1 1 5];
-lonscale = mean(cos(swift.lat*pi/180),'omitnan');
-scatter(swift.lon,swift.lat,[],swift.time,'filled');
-set(gca,'YAxisLocation','right')
-hold on
-quiver(swift.lon,swift.lat,swift.driftu./lonscale,swift.driftv,'k')
-c = slimcolorbar;
-c.Location = 'South';
-c.TickLabels = datestr(c.Ticks,'mmm-dd');
-ylabel('Lat [^{\circ}N]')
-xlabel('Lon [^{\circ}E]')
-set(gca,'XAxisLocation','top')
-axis equal square
-textlab(['SWIFT ' SWIFT(1).ID],'topleft')
-
-% Waves Spectra
-axes(h(18))
-ax = gca;ax.Position = ax.Position.*[1 1 1 2];
-cwind = cmocean('matter',18);
-ubin = [0 2:18 50];
-for it = 1:length(swift.time)
-    if ~isnan(swift.windu(it))
-    [~,~,ibin] = histcounts(swift.windu(it),ubin);
-    if ibin ~= 0
-    plot(swift.wavefreq,swift.wavepower(:,it),'color',cwind(ibin,:),'LineWidth',2)
-    hold on
-    else
-        plot(swift.wavefreq,swift.wavepower(:,it),'k','LineWidth',2)
-    end
-    else
-        plot(swift.wavefreq,swift.wavepower(:,it),'k','LineWidth',2)
-    end
-end
-xlim([0.05 0.5])
-set(gca,'YScale','log')
-colormap(gca,cwind)
-c = slimcolorbar;
-c.Label.String = 'U [ms^{-1}]';
-c.Ticks = 0:0.25:1;
-c.TickLabels = num2str((c.Ticks*16 + 2)');
-xlabel('F [Hz]')
-ylabel('P_{w} [m^2Hz^{-1}]')
-title('Wave Spectra')
-set(gca,'YTick',10.^([-4:2:1]))
-
-axes(h(24))
-ax = gca;ax.Position = ax.Position.*[1 1 1 2];
-pcolor(swift.time,swift.wavefreq,log10(swift.wavepower))
-shading flat
-set(gca,'YScale','log')
-c = slimcolorbar;
-c.Label.String = 'P_{w} [m^2Hz^{-1}]';
-set(gca,'YDir','Reverse')
-ylim([0.05 0.5])
-set(gca,'YTick',0.1:0.1:0.5)
-ylabel('F [Hz]')
-
-linkaxes(h([1:3:end 2:3:end 24]),'x')
-set(h([1:3:end-3 2:3:end-3]),'XTickLabel',[])
-axes(h(22))
-axis tight
-datetick('x','KeepLimits')
-axes(h(23))
-datetick('x','KeepLimits')
-axes(h(24))
-datetick('x','KeepLimits')
-
-set(h,'FontSize',12)
-rmemptysub
 
