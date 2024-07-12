7c876cfcd9da12b9c321980a6358a739b65395c6
Michael James <mikejames.mj867@gmail.com>
2024-07-12 22:10:11 +0000

    Adding Percentile Outlier finder and hist analysis to aid SWIFT_QC

---
+++ GeneralTools/findoutliersSWIFT.m
@@ -0,0 +1,11 @@
+function [SWIFT,outliersSWIFT] = findoutliersSWIFT(flist`)
+%findoutliersSWIFT - finds outliers out of % range and plots hist of all
+%SWIFT fields
+% Find outliers in SWIFT L1 DATA    
+% Michael James
+% 7_12_2024
+
+
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
