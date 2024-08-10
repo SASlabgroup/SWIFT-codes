bf8b97602f1892634a7a0540f71e56e3ede68f00
Jim Thomson <jthomson@apl.washington.edu>
2024-08-09 22:07:34 -0700

    Update SWIFTmss.m

--- Waves/SWIFTmss.m
+++ Waves/SWIFTmss.m
@@ -1,35 +1,43 @@
-function [mss, mssnorm, SWIFT] = SWIFTmss( SWIFT, plotflag )
+function [mss_total, mss_tail, mss_tail_norm, SWIFT] = SWIFTmss( SWIFT, fixedfrangeflag, plotflag )
 % function to calculate mean square slope from the wave spectra in a SWIFT data structure
 % outputs are mss, and mss normalize by the width of the frequency range 
 %
-%   [mss, mssnorm] = SWIFTmss( SWIFT );
+%  [mss_total, mss_tail, mss_tail_norm, SWIFT] = SWIFTmss( SWIFT, fixedfreqrange, plotflag )
 %
 % J. Thomson, Sept 2020
-%               Nov 2021, use dynamic frequency range (1.0 f_e to 2.0 f_e)
+%               Nov 2021, use dynamic frequency range (f > f_e)
 %                       also expand results to include theoretical mss and put results in SWIFT structure
 %                   and add a plotting option
+%               Aug 2024, add option (binary flag) for fixed frequency range
+%                   rename output as mss from high freq tail of spectra
+%                   add total mss as another output
 
-%fmin = 0.3; % fixed options (legacy)
-%fmax = 0.45; % max freq (legacy)
+fmin = 0.2; % fixed options (legacy)
+fmax = 0.4; % max freq (legacy)
 
-if isfield(SWIFT,'wavespectra'),
-    for si = 1:length(SWIFT), 
+if isfield(SWIFT,'wavespectra')
+    for si = 1:length(SWIFT)
         
         %% observed mss (as 4th moment of spectral tail)
         df = mean(diff( SWIFT(si).wavespectra.freq ));
         fe(si) = nansum(SWIFT(si).wavespectra.freq .* SWIFT(si).wavespectra.energy) ./ nansum(SWIFT(si).wavespectra.energy) ;
-        findices = find( SWIFT(si).wavespectra.freq > fe(si) );
-        mss(si) = nansum( (2*3.14*SWIFT(si).wavespectra.freq(findices)).^4 .* SWIFT(si).wavespectra.energy(findices) ) .* df ./ (9.8^2);
-        mssnorm(si) = mss(si) ./ range( SWIFT(si).wavespectra.freq(findices) );
+        if fixedfrangeflag
+            findices = find( SWIFT(si).wavespectra.freq > fmin & SWIFT(si).wavespectra.freq < fmax);
+        else
+            findices = find( SWIFT(si).wavespectra.freq > fe(si) );
+        end
+        mss_total(si) = nansum( (2*3.14*SWIFT(si).wavespectra.freq(:)).^4 .* SWIFT(si).wavespectra.energy(:) ) .* df ./ (9.8^2);
+        mss_tail(si) = nansum( (2*3.14*SWIFT(si).wavespectra.freq(findices)).^4 .* SWIFT(si).wavespectra.energy(findices) ) .* df ./ (9.8^2);
+        mss_tail_norm(si) = mss_tail(si) ./ range( SWIFT(si).wavespectra.freq(findices) );
         
         %% adjust the mss observations for relative currents, see Iyer et al 2021/2022
         % ** some confusion over normalization and sign of adjustment
         if isfield(SWIFT,'driftspd')
             recip = 0; % wave direction towards, not from
-            [Etheta theta E f dir spread spread2 spread2alt ] = SWIFTdirectionalspectra(SWIFT(si), 0, recip); 
+            [Etheta, theta, E, f, dir, spread, spread2, spread2alt ] = SWIFTdirectionalspectra(SWIFT(si), 0, recip); 
             EqDir(si) = nansum(E(findices) .* dir(findices) ) ./ nansum(E(findices));
             relU(si) = SWIFT(si).driftspd * cosd( EqDir(si) - SWIFT(si).driftdirT )  % relative current (sign matters!)
-            mssadjusted(si) = mss(si) - nansum( 8*3.14*SWIFT(si).wavespectra.freq(findices) * relU(si) ) .* df ./ (9.8) ... 
+            mssadjusted(si) = mss_tail(si) - nansum( 8*3.14*SWIFT(si).wavespectra.freq(findices) * relU(si) ) .* df ./ (9.8) ... 
                 -  nansum( 24*(3.14*SWIFT(si).wavespectra.freq(findices) * relU(si) ).^2 ) .* df ./ (9.8^2) ... 
                 -  nansum( 32*(3.14*SWIFT(si).wavespectra.freq(findices) * relU(si) ).^3 ) .* df ./ (9.8^3) ... 
                 -  nansum( 16*(3.14*SWIFT(si).wavespectra.freq(findices) * relU(si) ).^4 ) .* df ./ (9.8^4) ;
@@ -52,8 +60,9 @@ if isfield(SWIFT,'wavespectra'),
         
         %% put results in data structure
         SWIFT(si).centriodwaveperiod = 1./fe(si);
-        SWIFT(si).mss = mss(si);
-        SWIFT(si).mssnormalized = mssnorm(si);
+        SWIFT(si).mss_total = mss_total(si);
+        SWIFT(si).mss_tail = mss_tail(si);
+        SWIFT(si).mss_tail_norm = mss_tail_norm(si);
         SWIFT(si).mssadjusted = mssadjusted(si);
         SWIFT(si).msstheoretical = msstheoretical(si);
         if mssadjusted(si) < 0, 
@@ -62,15 +71,16 @@ if isfield(SWIFT,'wavespectra'),
 
     end
 else
-    mss = NaN(length(SWIFT));
-    mssnorm = NaN(length(SWIFT));
+    mss_total = NaN(length(SWIFT));
+    mss_tail = NaN(length(SWIFT));
+    mss_tail_norm = NaN(length(SWIFT));
 end
 
 
 if plotflag,
     %%
     figure(1), clf
-    plot([SWIFT.time],[SWIFT.mss],':',[SWIFT.time],[SWIFT.mssnormalized],[SWIFT.time],[SWIFT.mssadjusted],[SWIFT.time], [SWIFT.msstheoretical],'linewidth',3)
+    plot([SWIFT.time],[SWIFT.mss_tail],':',[SWIFT.time],[SWIFT.mss_tail_norm],[SWIFT.time],[SWIFT.mssadjusted],[SWIFT.time], [SWIFT.msstheoretical],'linewidth',3)
     datetick
     ylabel('mss []')
     legend('observed mss','observed mss, normalized','adjusted mss','theoretical mss, normalized','Location','NortheastOutside')
