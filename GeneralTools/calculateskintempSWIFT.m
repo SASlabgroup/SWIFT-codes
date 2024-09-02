1696490f405a44629a8cfeeabdac569a8275052e
unknown <mikejames.mj867@gmail.com>
2024-09-02 12:40:10 -0700

    added in function to calculate skin temperature using up and downlooking radiometer data on SWIFT

---
+++ GeneralTools/calculateskintempSWIFT.m
@@ -0,0 +1,7 @@
+function [SWIFT.SWrad,SWIFT.LWrad] = calculateskintempSWIFT(SWIFT)
+%calculateskintempSWIFT Takes uplooking and downlooking point radiometer
+%data and calculates skin temp
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
