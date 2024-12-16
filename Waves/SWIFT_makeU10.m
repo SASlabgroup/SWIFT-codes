7354a9b73de6ef27ea658d5b46b1f7cbfcc5b9d1
unknown <mikejames.mj867@gmail.com>
2024-12-16 12:45:41 -0800

    Initial add of U10 approximation

---
+++ Waves/SWIFT_makeU10.m
@@ -0,0 +1,26 @@
+function SWIFT=SWIFT_makeU10(SWIFT)
+%%%%%%%%%%%%%%%%%SWIFT_makeU10.m
+%
+%   Calculates 10 m winds from SWIFT.windspd, follows the code established
+%   in COARE 3.6 algorithm. https://github.com/NOAA-PSL/COARE-algorithm. 
+% 
+%   Scientifically, this adjustment follows the log layer assumption of 
+%   the wind profile, where vertical gradient of air density == 0;
+%
+%   Created: M. James, December 2024
+
+if isfield(SWIFT, 'windspd');
+    if isfield(SWIFT, 'metheight');
+        for k=1:length(SWIFT);
+            zu = SWIFT(k).metheight;
+            SWIFT(k).windspd10 = SWIFT(k).windspd.*log(10/1e-4)./log(zu/1e-4);
+        end
+    else
+        error("Need metheight input")
+    end
+else
+    error("Need windspd input")
+end
+
+
+end
