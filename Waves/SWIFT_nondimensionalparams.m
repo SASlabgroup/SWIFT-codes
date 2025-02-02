faf7c8ede51d035788e78b2144aecea5ddb31461
unknown <mikejames.mj867@gmail.com>
2025-02-02 14:01:01 -0800

    Nondimensional params improvements

--- Waves/SWIFT_nondimensionalparams.m
+++ Waves/SWIFT_nondimensionalparams.m
@@ -9,6 +9,8 @@ function [nondim, SWIFT] =SWIFT_nondimensionalparams(SWIFT,plotbool)
 %
 %   Created: M. James, December 2024
 
+name = 'SWIFT20L2-waveagefiltered';
+
 % Load in coastline database from GSHHG (Alaska coast available from M. James)
 load("Coastlinelatlon.mat");
 
@@ -20,8 +22,9 @@ end
 % SWIFTfetch calculation
 tic;sprintf("Calculating Fetch, time elapsed %ds",round(toc))
 if ~isfield(SWIFT,'winddirT')
-    warning('No true winds, using relative winds');
-    winddir = [SWIFT.peakwavedirT]; % use wave dir
+    warning('No true winds, using wave dir');
+    winddir = mod([SWIFT.peakwavedirT]+180, 360); % use wave dir
+    % winddir = [SWIFT.peakwavedirT];
 
     %% Calculation of true winds from wdirR and driftdirT, code in progress...
     % for k = 1:length(SWIFT);
@@ -39,15 +42,49 @@ end
 % Fit fetchbins to data (ASSUMING Stationary comapared to fetch distance
 % ds << fetch)
 
-idx = find(abs([SWIFT.lat] -nanmean([SWIFT.lat]))+ abs([SWIFT.lon] -nanmean([SWIFT.lon]))...
-    == min(abs([SWIFT.lat] -nanmean([SWIFT.lat]))+ abs([SWIFT.lon] -nanmean([SWIFT.lon]))));% find point closest to center
-[fetchbins,degbincenter]=SWIFT_makefetchbins(SWIFT(idx).lat,SWIFT(idx).lon, coastlinelatlon)
+% % Works for stationary
+% idx = find(abs([SWIFT.lat] -nanmean([SWIFT.lat]))+ abs([SWIFT.lon] -nanmean([SWIFT.lon]))...
+%     == min(abs([SWIFT.lat] -nanmean([SWIFT.lat]))+ abs([SWIFT.lon] -nanmean([SWIFT.lon]))));% find point closest to center
+% [fetchbins,degbincenter]=SWIFT_makefetchbins(SWIFT(idx).lat,SWIFT(idx).lon, coastlinelatlon)
+% 
+% clear idx
+% for k = 1:length(SWIFT)
+%     [~, idx] = min(abs(winddir(k) - degbincenter));
+%     SWIFT(k).fetch = fetchbins(idx);
+%     clear idx;
+% end
+
+% Adapted to moving buoys
+% Bin everything to 9 deg bins
+bincenters = [0,4.5:360]';
+[~, idx] = min(abs(winddir - bincenters),[],1);
+winddir = bincenters(idx);
 
 clear idx
 for k = 1:length(SWIFT)
-    [~, idx] = min(abs(winddir(k) - degbincenter));
-    SWIFT(k).fetch = fetchbins(idx);
-    clear idx;
+    %Define radius of fetch calc
+    r = 1000; %km
+    r = km2deg(r);
+    
+    % Set up wind vector
+    windvx = [SWIFT(k).lon, SWIFT(k).lon + sind(winddir(k)).*r]; % x ccw from north
+    windvy = [SWIFT(k).lat, SWIFT(k).lat + cosd(winddir(k)).*r]; % y ccw from north
+   
+    % 1 --> 2 first line, 3 --> 4 second line
+    % Find intersection points
+    [xi, yi] = polyxpoly(windvx, windvy, ...
+        coastlinelatlon.lon, coastlinelatlon.lat);
+
+    % Check if there is an intersection
+    isIntersect = ~isempty(xi);
+
+    if isIntersect
+        distance = sqrt((xi-SWIFT(k).lon).^2 + (yi-SWIFT(k).lat).^2);
+        SWIFT(k).fetch = deg2km(min(distance)); % kmclear
+    else
+        SWIFT(k).fetch =NaN;
+    end
+    clear idx xi yi isIntersect distance
 end
 
 
@@ -69,27 +106,28 @@ nondim.sigH = g.^2.*[SWIFT.sigwaveheight]' ./ (([SWIFT.windspd10]').^2);
 
 nondim = struct2table(nondim);
 
-name = 'SWIFT20-waveagefiltered';
-
 if plotbool
     disp('Plot flag true... plotting results')
     % Limits
-    epmax = 3.6e-3;
+    epmax = 3.6e-3; % from young 1999
     vmin = 0.13;
     sigHmax = 0.15;
 
 
     figure
     histogram(nondim.pkf,30)
-    hold on
-    xline(1/(2*pi),'k','LineWidth',2)
-    xline(vmin,'k--','LineWidth',2)
-    legend('','Wave Age Minimum for Non Swell','Fully Developed Minimum')
-    title('Histogram of Nondimensional Peak Frequency')
-
-    
+        
     disp('Plotting filtered by wave age for pure wind seas')
     nondim = nondim(nondim.pkf > 1/(2*pi),:); % Filter out by wave age
+
+    hold on
+    histogram(nondim.pkf,30)
+
+    xline(1/(2*pi),'k','LineWidth',2)
+    xline(vmin,'k--','LineWidth',2)
+    legend('','waveagefilter','Wave Age Minimum for Non Swell','Fully Developed Minimum')
+    title('Histogram of Nondimensional Peak Frequency')
+
     figure
 
     % Fetch vs Energy
@@ -105,7 +143,7 @@ if plotbool
     plot(nondim.fetch, 10.^polyval(p, log10(nondim.fetch)),'r','DisplayName',regressslope)
     yline(epmax,'k--','DisplayName','Fully Developed max')
     set(gca, 'XScale','log');set(gca, 'YScale','log');
-    legend()
+    legend('Location','northwest')
     xlabel(' \chi')
     ylabel(' \epsilon ')
 
@@ -140,7 +178,7 @@ if plotbool
     plot(nondim.fetch, 10.^polyval(p, log10(nondim.fetch)),'r','DisplayName',regressslope)
     yline(sigHmax,'k--','DisplayName','Fully Developed max')
     set(gca, 'XScale','log');set(gca, 'YScale','log');
-    legend()
+    legend('Location','northwest')
     xlabel(' \chi')
     ylabel('$\hat{H}$', 'Interpreter', 'latex')
 
