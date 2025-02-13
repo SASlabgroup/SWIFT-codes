c157d0a61f4f26ef4b41f71791292dada3ad444e
unknown <mikejames.mj867@gmail.com>
2025-02-13 15:11:37 -0800

    Compiled COARE files all to one folder in generaltools, added COARE_quality script to analyze COARE output % errors

---
+++ GeneralTools/COAREwSWIFT/COARE_quality.m
@@ -0,0 +1,6 @@
+function [qual] = COARE_quality(SWIFT, fluxes)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
