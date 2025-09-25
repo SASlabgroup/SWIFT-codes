aef2a3d8b584777804daf088f651cb52d531dfad
Jim Thomson <jthomson@apl.washington.edu>
2025-09-25 06:46:21 -0700

    Update plotmultipleSWIFTs_timeseries.m

--- GeneralTools/plotmultipleSWIFTs_timeseries.m
+++ GeneralTools/plotmultipleSWIFTs_timeseries.m
@@ -6,41 +6,45 @@ clear all, close all
 
 fpath = './';
 
-filelist = dir('*SWIFT*.mat');
+flist = dir('*SWIFT*.mat');
 
+figure(1), 
+cmap = colormap;
 
-for fi=1:length(filelist)
+for fi=1:length(flist)
 
-    load(filelist(fi).name)
+    load(flist(fi).name)
+    thiscolor = cmap(round(fi/length(flist)*256),:);
+    %SWIFT( [SWIFT.sigwaveheight]==0 ) = [];
 
     subplot(4,1,1)
     if isfield(SWIFT, 'windspd')
-        plot([SWIFT.time],[SWIFT.windspd],'x'), hold on
+        plot([SWIFT.time],[SWIFT.windspd],'x','color',thiscolor,'linewidth',2,'linewidth',2), hold on
     else
-        plot([SWIFT.time],NaN,'x'), hold on
+        plot([SWIFT.time],NaN,'x','color',thiscolor,'linewidth',2), hold on
     end
     datetick
     ylabel('Wind spd [m/s]')
 
     subplot(4,1,2)
-    plot([SWIFT.time],[SWIFT.sigwaveheight],'x'), hold on
+    plot([SWIFT.time],[SWIFT.sigwaveheight],'x','color',thiscolor,'linewidth',2), hold on
     datetick
     ylabel('Wave height [m]')
 
     subplot(4,1,3)
     if isfield(SWIFT,'watertemp')
-        plot([SWIFT.time],[SWIFT.watertemp],'x'), hold on
+        plot([SWIFT.time],[SWIFT.watertemp],'x','color',thiscolor,'linewidth',2), hold on
     else
-        plot([SWIFT.time],NaN,'x'), hold on
+        plot([SWIFT.time],NaN,'x','color',thiscolor,'linewidth',2), hold on
     end
     datetick
     ylabel('Water temp [C]')
 
     subplot(4,1,4)
     if isfield(SWIFT,'salinity')
-        plot([SWIFT.time],[SWIFT.salinity],'x'), hold on
+        plot([SWIFT.time],[SWIFT.salinity],'x','color',thiscolor,'linewidth',2), hold on
     else
-        plot([SWIFT.time],NaN,'x'), hold on
+        plot([SWIFT.time],NaN,'x','color',thiscolor,'linewidth',2), hold on
     end
     datetick
     ylabel('Salinity [PSS]')
