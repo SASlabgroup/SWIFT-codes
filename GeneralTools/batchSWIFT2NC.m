90c49c5e2e978043b09c0d4599d14945295f9ca6
mleclair73 <25091937+mleclair73@users.noreply.github.com>
2025-11-01 14:39:37 -0700

    Update batchSWIFT2NC with glob, better error report (#76)

--- GeneralTools/batchSWIFT2NC.m
+++ GeneralTools/batchSWIFT2NC.m
@@ -1,19 +1,65 @@
+function batchSWIFT2NC(varargin)
 % Matlab script to convert a batch of SWIFT matlab structures into NetCDF
 %
 % J. Thomson, May 2021
+% M. LeClair, Oct 2025
+%
+% Usage:
+%   batchSWIFT2NC    % default: '*SWIFT*.mat' in current dir
+%   batchSWIFT2NC('glob_path', '*.mat')
 
-close all, clear all
+% Parse inputs
+p = inputParser;
+addParameter(p, 'glob_path', '*SWIFT*.mat', @ischar);
+parse(p, varargin{:});
 
-flist = dir('*SWIFT*.mat');
+glob_path = p.Results.glob_path;
 
-for fi=1:length(flist)
-    try
-        load(flist(fi).name,'SWIFT')
-        SWIFT2NC(SWIFT,[ flist(fi).name(1:end-4) '.nc'] )
-        fprintf('Successfully processed: %s\n', flist(fi).name);
-    catch ME
-        fprintf('Error processing %s: %s\n', flist(fi).name, ME.message);
-        fprintf('  at line %d in %s\n', ME.stack(1).line, ME.stack(1).name);
+flist = dir(glob_path);
+
+if isempty(flist)
+    fprintf('No files found matching: %s\n', glob_path);
+    return;
+end
+
+fprintf('Found %d file(s) matching "%s":\n', length(flist), glob_path);
+fprintf('Processing:\n');
+
+for fi = 1:length(flist)
+    clear SWIFT
+    
+    % Generate output filename in current directory
+    [~, name, ~] = fileparts(flist(fi).name);
+    nc_filename = [name '.nc'];
+    
+    fprintf('  %d. %s -> %s\n', fi, flist(fi).name, nc_filename);
+    
+    % Load SWIFT or SIG structure
+    data = load(flist(fi).name);
+    
+    if isfield(data, 'SWIFT')
+        SWIFT = data.SWIFT;
+    elseif isfield(data, 'SIG')
+        fprintf('       SIG structure not yet supported\n');
+        continue
+    else
+        fprintf('       File does not contain SWIFT or SIG structure\n');
         continue;
     end
+    
+    % If we found one, convert to NC
+    if exist('SWIFT', 'var')
+        try
+            SWIFT2NC(SWIFT, nc_filename);
+            fprintf('       Successfully processed\n');
+        catch ME
+            fprintf('       Error: %s\n', ME.message);
+            fprintf('              at line %d in %s\n', ME.stack(1).line, ME.stack(1).name);
+            fprintf('       Command to reproduce: SWIFT2NC(SWIFT, ''%s'')\n', nc_filename);
+        end
+    else
+        fprintf('         File does not contain a SWIFT structure\n');
+    end
+end
+
 end
