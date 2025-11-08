cb5715cc832c55bbf07d4704ea2102bc3b48f67c
Malcolm <malcolmleclair@gmail.com>
2025-11-08 14:04:36 -0800

    Add obs to swift2nc

--- GeneralTools/SWIFT2NC.m
+++ GeneralTools/SWIFT2NC.m
@@ -147,7 +147,27 @@ if isfield(SWIFT,'signature')
         z_names = [];
     end
 end
-
+obs_dim = -1;
+obs_size = -1;
+if isfield(SWIFT, 'OBS_uncalibrated')
+    obs_dim = netcdf.defDim(ncid,'obs_sample', length(SWIFT(1).OBS_uncalibrated));
+    obs_size = length(SWIFT(1).OBS_uncalibrated);
+end
+if isfield(SWIFT, 'OBS_ambient')
+    if obs_dim == -1
+        obs_dim = netcdf.defDim(ncid,'obs_sample', length(SWIFT(1).OBS_ambient));
+        obs_size = length(SWIFT(1).OBS_ambient);
+    elseif obs_size ~= length(SWIFT(1).OBS_ambient)
+        error('bad obs size')
+    end
+end
+if isfield(SWIFT, 'OBS_calibratedNTU')
+    if obs_dim == -1
+        obs_dim = netcdf.defDim(ncid,'obs_sample', length(SWIFT(1).OBS_calibratedNTU));
+    elseif obs_size ~= length(SWIFT(1).OBS_calibratedNTU);
+        error('bad obs size')
+    end
+end
 
 
 j=1;
@@ -184,9 +204,14 @@ end
 
 
 %% creating netcdf variables
-
 for i=1:length(names)
-    if strcmp(names{i},'wavespectra')
+    if strcmp(names{i},'OBS_uncalibrated')
+        var_ids.(names{i})  = netcdf.defVar(ncid, 'OBS_uncalibrated', 'NC_INT', [obs_dim, t_dim]);
+    elseif strcmp(names{i},'OBS_ambient')
+        var_ids.(names{i})  = netcdf.defVar(ncid, 'OBS_ambient', 'NC_INT', [obs_dim, t_dim]);
+    elseif strcmp(names{i},'OBS_calibratedNTU')
+        var_ids.(names{i}) = netcdf.defVar(ncid, 'OBS_calibratedNTU', 'NC_INT', [obs_dim, t_dim]);
+    elseif strcmp(names{i},'wavespectra')
         for j=1:length(spec_names)
             if strcmp(spec_names{j},'freq')
                 spec_var_ids.(spec_names{j}) = netcdf.defVar(ncid, spec_names{j}, 'NC_DOUBLE', f_dim);
@@ -347,7 +372,13 @@ netcdf.endDef(ncid);
 %% filling them with values
 
 for i=1:length(names)
-    if strcmp(names{i},'wavespectra') & exist('spec_names', 'var')
+    if strcmp(names{i},'OBS_uncalibrated')
+        netcdf.putVar(ncid, var_ids.(names{i}), S.OBS_calibratedNTU.')
+    elseif strcmp(names{i},'OBS_calibrated')
+        netcdf.putVar(ncid, var_ids.(names{i}), S.OBS_calibratedNTU.')
+    elseif strcmp(names{i},'OBS_calibratedNTU')
+        netcdf.putVar(ncid, var_ids.(names{i}), S.OBS_calibratedNTU.')
+    elseif strcmp(names{i},'wavespectra') & exist('spec_names', 'var')
         for j=1:length(spec_names)
             if strcmp(spec_names{j},'freq')
                 netcdf.putVar(ncid, spec_var_ids.(spec_names{j}), S.wavespectra(ref_idx).freq);
@@ -410,9 +441,7 @@ for i=1:length(names)
             end
         end
     else
-
         netcdf.putVar(ncid, var_ids.(names{i}), S.(names{i}));
-
     end
 end
 
