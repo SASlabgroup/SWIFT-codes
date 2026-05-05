fa7b18c64e580a0f831efa943815da57a82efe01
Malcolm <malcolmleclair@gmail.com>
2026-05-04 21:20:05 -0700

    permissive plot

--- GeneralTools/SWIFT2NC.m
+++ GeneralTools/SWIFT2NC.m
@@ -1,4 +1,4 @@
-function SWIFT2NC(SWIFT_in,filename)
+function SWIFT2NC(SWIFT_in,filename, temp)
 %
 % creates a netCDF file using existing SWIFT structure and writes it into 'filename'
 % (must include .nc)
