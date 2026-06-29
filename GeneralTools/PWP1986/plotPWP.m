c393d3566717057a03a57bfb513407858e8c3427
Michael James <mikejames.mj867@gmail.com>
2026-06-29 15:08:26 -0700

    Added plotting tool for timeseries inputs and outputs

---
+++ GeneralTools/PWP1986/plotPWP.m
@@ -0,0 +1,13 @@
+function [fig] = plotPWP(pwp_struct, name)
+%plotPWP Plots all vars in a pwp structure
+%   Takes any array, ideally pwp_input or pwp_output and makes plots of
+%   each variable as a timeseries. 
+%   
+%   Requires "time" variable to be able to plot
+
+
+
+
+
+
+end
